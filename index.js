83e18f0f2cba1a5c64e31af7309ab75c5960c08a
mikeng9944 <mike.ng@gti.com.hk>
2024-03-26 18:44:36 +0800

    update code

--- index.js
+++ index.js
@@ -6,32 +6,37 @@ const puppeteer = require("puppeteer");
 
 app.get("/refresh", function (req, res) {
   (async () => {
-    const browser = await puppeteer.connect({
-      browserURL: "http://127.0.0.1:9222",
-    });
-    const page = await browser.newPage();
+    try {
+      const browser = await puppeteer.connect({
+        browserURL: "http://127.0.0.1:9222",
+      });
+      const page = await browser.newPage();
 
-    await page.goto(
-      "https://app.powerbi.com/groups/ce2aea02-18eb-4751-a383-4bfe0457272b/datasets/0a5183b0-0ffe-4b09-84d2-3bbe6acd5aec/details?experience=power-bi"
-    );
-    // await page.waitForSelector(
-    //   'div[data-test-id="mike.ng@valuepartners-group.com"]'
-    // );
-    // await page.click('div[data-test-id="mike.ng@valuepartners-group.com"]');
-    await page.waitForSelector('button[title="Refresh"]');
-    await page.click('button[title="Refresh"]');
-    await page.waitForSelector('button[title="Refresh now"]', {
-      visible: true,
-    });
-    await page.click('button[title="Refresh now"]');
-    await page.click('button[title="Refresh now"]');
-    await page.click('button[title="Refresh now"]');
-    await page.click('button[title="Refresh now"]');
+      await page.goto(
+        "https://app.powerbi.com/groups/ce2aea02-18eb-4751-a383-4bfe0457272b/datasets/0a5183b0-0ffe-4b09-84d2-3bbe6acd5aec/details?experience=power-bi"
+      );
+      // await page.waitForSelector(
+      //   'div[data-test-id="mike.ng@valuepartners-group.com"]'
+      // );
+      // await page.click('div[data-test-id="mike.ng@valuepartners-group.com"]');
+      await page.waitForSelector('button[title="Refresh"]');
+      await page.click('button[title="Refresh"]');
+      await page.waitForSelector('button[title="Refresh now"]', {
+        visible: true,
+      });
+      await page.click('button[title="Refresh now"]');
+      await page.click('button[title="Refresh now"]');
+      await page.click('button[title="Refresh now"]');
+      await page.click('button[title="Refresh now"]');
 
-    setTimeout(function () {
-      page.close();
-    }, 5000);
-    res.send("Finished refresh.");
+      setTimeout(function () {
+        page.close();
+      }, 5000);
+      res.send("Finished refresh.");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("An error occurred while refreshing.");
+    }
   })();
 });
 
