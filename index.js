db910294bae31bf00a355a7264bd3563ca11f819
mikeng9944 <mike.ng@gti.com.hk>
2024-03-26 11:42:48 +0800

    debug

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.get("/refresh", function (req, res) {
       headless: false,
     });
     const page = await browser.newPage();
+
+    await page.setExtraHTTPHeaders({
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+      "upgrade-insecure-requests": "1",
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+      "accept-encoding": "gzip, deflate, br",
+      "accept-language": "en-US,en;q=0.9,en;q=0.8",
+    });
+
     await page.goto(
       "https://app.powerbi.com/groups/ce2aea02-18eb-4751-a383-4bfe0457272b/datasets/0a5183b0-0ffe-4b09-84d2-3bbe6acd5aec/details?experience=power-bi"
     );
